89e8cbd421c3c5adc01c655a3eab78c181cabe31
kravchenko-pavel <kpavel@il.ibm.com>
2014-09-01 13:05:14 +0300

    first commit

--- test_init.sh
+++ test_init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mkdir /var/log/saved1/
-cp -r /root/* /var/log/saved1/
+sudo apt-get install -y curl
+
+curl https://api.service.softlayer.com/rest/v3/SoftLayer_Resource_Metadata/UserMetadata.txt > ~/metadata.txt
 
 cat /var/lib/heat-cfntools/hack/userdata_static > /var/lib/heat-cfntools/hack/userdata
+
 curl https://api.service.softlayer.com/rest/v3/SoftLayer_Resource_Metadata/UserMetadata.txt >> /var/lib/heat-cfntools/hack/userdata
 
 rm -rf /var/lib/cloud/instances/
@@ -11,12 +13,6 @@ rm -rf /var/cache/heat-cfntools/*
 
 cloud-init init
 
-mkdir /var/log/saved2/
-cp -r /root/* /var/log/saved2/
-
 cloud-init modules --mode init
 cloud-init modules --mode config
 cloud-init modules --mode final
-
-mkdir /var/log/saved3/
-cp -r /root/* /var/log/saved3/
